cbad16f04718f878cde17cb92e2a8e6d1e4b571b
Fawaz <fawazming@gmail.com>
2024-10-05 08:43:33 +0100

    init

---
+++ app/Views/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>XS Mobile App by Rayyan Technologies</title>
+</head>
+<body>
+    <center><font size="6" face="verdana" color="blue">
+    XS Mobile App by Rayyan Technologies
+    </font></center>
+
+    <form action="" method="post">
+        <p>Search</p>
+        <input type="search" placeholder="Search anything">
+    </form>
+
+    <br><br><hr>
+    <center><font size="3" face="verdana" color="red">
+        List of Apps Available
+    </font></center>
+</body>
+</html>
